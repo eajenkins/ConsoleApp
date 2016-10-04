aa90276072e05dd2b18f8ace5ff546257c369e6e
REDMOND\ejenkins <e.a.jenkins@live.com>
2016-10-04 15:14:09 -0700

    Convert to Node test

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -24,32 +24,32 @@ namespace ConsoleApplication
 		{
 			string[] sarray = new string[0];
 
-			Console.WriteLine("1: {0}", TestSwitch(1));
-			Console.WriteLine("2: {0}", TestSwitch(2));
-			Console.WriteLine("0: {0}", TestSwitch(0));
-			Console.WriteLine("99: {0}", TestSwitch(99));
+			//Console.WriteLine("1: {0}", TestSwitch(1));
+			//Console.WriteLine("2: {0}", TestSwitch(2));
+			//Console.WriteLine("0: {0}", TestSwitch(0));
+			//Console.WriteLine("99: {0}", TestSwitch(99));
 
-			//List<Foo> foos = new List<Foo> { new Foo(1, "Help"), new Foo(2, "Boo") };
+			List<Foo> foos = new List<Foo> { new Foo(1, "Help"), new Foo(2, "Boo") };
 
-			//Node root = new Node(1, "Hello");
-			//Node[] nodes = new Node[] { new Node(2, "Cloud"), new Node (3, "Piano"),
-			//new Node(4, "Syntax"), new Node(5, "Nucleus"), new Node(6, "Ship"), new Node(7, "DNA") };
+			Node root = new Node(1, "Hello");
+			Node[] nodes = new Node[] { new Node(2, "Cloud"), new Node (3, "Piano"),
+			new Node(4, "Syntax"), new Node(5, "Nucleus"), new Node(6, "Ship"), new Node(7, "DNA") };
 
-			//foreach (var node in nodes)
-			//{
-			//	Insert(root, node);
-			//}
+			foreach (var node in nodes)
+			{
+				Insert(root, node);
+			}
 
-			//Console.Write("Root: ");
-			//Print(root, 0);
+			Console.Write("Root: ");
+			Print(root, 0);
 
-			//foos.ForEach(x => Console.WriteLine($"Bar = {x.Bar}, Bam = {x.Bam}"));
+			foos.ForEach(x => Console.WriteLine($"Bar = {x.Bar}, Bam = {x.Bam}"));
 
-			//Foo simple = new Foo(3, "blah");
+			Foo simple = new Foo(3, "blah");
 
-			//string myFoo = String.Format($"Bar = {simple.Bar}, Bam = {simple.Bam}");
+			string myFoo = String.Format($"Bar = {simple.Bar}, Bam = {simple.Bam}");
 
-			//Console.WriteLine("{0}", myFoo);
+			Console.WriteLine("{0}", myFoo);
 
 			Console.ReadKey();
 		}
@@ -79,7 +79,7 @@ namespace ConsoleApplication
 		static void Print(Node root, int depth)
 		{
 			Console.WriteLine($"{root.Description}-{depth}");
-            if (root.Left != null)
+			if (root.Left != null)
 			{
 				Console.Write("Left: ");
 				Print(root.Left, depth + 1);
